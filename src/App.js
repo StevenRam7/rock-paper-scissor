c9659b3994d431794f60647bfcb77ae4f3d5901e
Steven <svramirez20@gmail.com>
2022-02-13 20:59:53 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Pick One!</h1>
-    <div className="options">
-      <button>ROCK</button>
-      <button>PAPER</button>
-      <button>SCISSORS</button>
-    </div>
+
+      <div className="options">
+        <button>ROCK</button>
+        <button>PAPER</button>
+        <button>SCISSORS</button>
+      </div>
     </div>
   );
 }
