df103031c02dd9599c66ac6621e453189c21926e
Steven <svramirez20@gmail.com>
2022-02-14 03:17:54 -0500

    added game file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import pic from "./rock-paper-scissors.jpg";
 
 function App() {
+  const [selection, setSelection] = useState();
+
+  //add useEffect
+
   return (
     <div>
       <h1>Pick One!</h1>
 
       <div className="options">
-        <button>ROCK</button>
-        <button>PAPER</button>
-        <button>SCISSORS</button>
+        <button onClick={() => setSelection("Rock")}>ROCK</button>
+        <button onClick={() => setSelection("Paper")}>PAPER</button>
+        <button onClick={() => setSelection("Scissors")}>SCISSORS</button>
       </div>
       <img src={pic} alt="rock paper and scissors" />
     </div>
