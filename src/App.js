3c6db11607a9ec367aaf9f6dac754c729c996f2a
Steven <svramirez20@gmail.com>
2022-02-14 22:52:16 -0500

    files rearranged, versus page added, not functional yet

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import "./App.css";
 import pic from "./rock-paper-scissors.jpg";
 
 function App() {
-  const [selection, setSelection] = useState();
+  const [selection, setSelection] = useState(null);
 
-  //add useEffect
+  const generateRandom = () => {
+    //generate computer's selection
+  }
 
   return (
-    <div>
-      <h1>Pick One!</h1>
+    <div className="select-view">
+      {selection === null ? (
+        <div>
+          <h1>Pick One!</h1>
 
-      <div className="options">
-        <button onClick={() => setSelection("Rock")}>ROCK</button>
-        <button onClick={() => setSelection("Paper")}>PAPER</button>
-        <button onClick={() => setSelection("Scissors")}>SCISSORS</button>
-      </div>
-      <img src={pic} alt="rock paper and scissors" />
+          <div className="options">
+            <button onClick={() => setSelection("Rock")}>ROCK</button>
+            <button onClick={() => setSelection("Paper")}>PAPER</button>
+            <button onClick={() => setSelection("Scissors")}>SCISSORS</button>
+          </div>
+          <img src={pic} alt="rock paper and scissors" />
+        </div>
+      ) : (
+        <div className="versus-view">
+          <h1>Versus!</h1>
+          <div className="versus">
+            <h2>Your choice: {selection}</h2>
+            {/* columns are uneven* */}
+            <h2 className="column2">Computer's choice: X</h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
